778dd5f309c3b0d5bb7fea537c0634284866163b
Parrottkim <parrottkim21@gmail.com>
2026-04-07 15:37:57 +0900

    fix: 출장 보고서 펼쳐보기가 잘못 표시되던 문제 해결
    close: #171

--- lib/src/presentation/screen/project/screen/report_list/widget/trip_preview_widget.dart
+++ lib/src/presentation/screen/project/screen/report_list/widget/trip_preview_widget.dart
@@ -92,7 +92,10 @@ class _DesktopWidget extends HookConsumerWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (auth is AuthAuthenticated && auth.user.id == item.user.id)
+          if (auth is AuthAuthenticated &&
+              auth.user.id == item.user.id &&
+              (item.schedule is ScheduleDomestic ||
+                  item.schedule is ScheduleOverseas))
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: TextButton.icon(
